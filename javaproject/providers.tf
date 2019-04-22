22ca107cd6e8f6afefe48151c9cd5a79a1c6c31c
Niranjan73 <46276169+Niranjan73@users.noreply.github.com>
2019-04-22 18:57:35 +0530

    Update providers.tf

--- javaproject/providers.tf
+++ javaproject/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "${var.aws_region}"
-  access_key = "AKIAZRCHFY6Q24FSUH4G"
-  secret_key = "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"
+  access_key = "AG"
+  secret_key = "MDjw"
 }
